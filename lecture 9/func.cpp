7b944f2d2165ef489ac39a12ca92f09445113d58
Lulky <codingblocks@Codings-MacBook-Pro.local>
2022-07-21 09:51:53 +0530

    lecture 9

---
+++ lecture 9/func.cpp
@@ -0,0 +1,99 @@
+#include<iostream>
+using namespace std;
+
+
+// int a(){
+// 	cout<<"a\n";
+// 	return 10;
+// }
+
+// void a(){
+// 	cout<<"a\n";
+// 	// return 10;
+// }
+
+// int a(){
+// 	return 10;
+// }
+
+// int z(){
+// 	return 7;
+// }
+
+// int z();
+
+// int a(int c, int d){
+// 	int e = z();
+// 	// cout<<c<<" "<<d<<endl;
+// 	cout<<"e: "<<e<<"\n";
+// 	return e;
+// }
+
+// void printworld(){
+// 	printhello();
+// 	cout<<"world\n";
+// }
+
+// int printa(int a){
+// 	// cout<<a<<endl;
+// 	return a;
+// } 
+void printworld();
+void printhello();
+
+int main(){
+	// int d = 4;
+	// int c = 5;
+	// int i = a(d, c);
+	// // cout<<i;
+	// i > 11 ? cout<<"greater than 11\n" : i > 5 ? cout<<"greater than 5 but smaller than 11\n" : cout<< "smaller than 5\n";
+
+
+
+
+
+	// int a = 10;
+	// if(a == 10){
+	// 	printa(a);
+	// 	// cout<<a;
+	// }
+
+	// cout<<"hello world;"
+
+	for(int i = 0; i < 5; i++){
+		printworld();
+	}
+
+
+
+
+	return 0;
+}
+
+
+void printworld(){
+	printhello();
+	cout<<"world\n";
+}
+void printhello(){
+	cout<<"hello ";
+}
+
+// int z(){
+// 	return 10;
+// }
+
+
+// https://hack.codingblocks.com/app/contests/3338
+
+
+
+
+
+
+
+
+
+
+
+
